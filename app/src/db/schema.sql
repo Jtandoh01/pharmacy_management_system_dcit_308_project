d7e11f944b6c54064ca83b518cd73ad3e289f550
Jtandoh01 <tandoh24700@gmail.com>
2024-06-23 14:33:14 +0000

    added more styling to components

--- app/src/db/schema.sql
+++ app/src/db/schema.sql
@@ -1,3 +1,4 @@
+-- Create the Drugs table
 CREATE TABLE Drugs (
     drugCode VARCHAR(50) PRIMARY KEY,
     name VARCHAR(100),
@@ -5,14 +6,14 @@ CREATE TABLE Drugs (
     quantity INT
 );
 
--- Creating the Suppliers table
+-- Create the Suppliers table
 CREATE TABLE Suppliers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL
-    );
+);
 
--- Creating the DrugSuppliers table to manage the many-to-many relationship
+-- Create the DrugSuppliers table to manage the many-to-many relationship
 CREATE TABLE DrugSuppliers (
     drugCode VARCHAR(255),
     supplierId INT,
@@ -21,17 +22,27 @@ CREATE TABLE DrugSuppliers (
     FOREIGN KEY (supplierId) REFERENCES Suppliers (id) ON DELETE CASCADE
 );
 
--- Creating the PurchaseHistory table
+-- Create the Customer table
+CREATE TABLE Customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contactNumber VARCHAR(20)
+);
+
+-- Update the PurchaseHistory table to include customer information
 CREATE TABLE PurchaseHistory (
     id SERIAL PRIMARY KEY,
     drugCode VARCHAR(255),
     purchaseDateTime TIMESTAMP NOT NULL,
     buyer VARCHAR(255) NOT NULL,
+    customerId INT,
     quantity INT NOT NULL,
     totalAmount DOUBLE PRECISION NOT NULL,
-    FOREIGN KEY (drugCode) REFERENCES Drugs (drugCode) ON DELETE CASCADE
+    FOREIGN KEY (drugCode) REFERENCES Drugs (drugCode) ON DELETE CASCADE,
+    FOREIGN KEY (customerId) REFERENCES Customers (id) ON DELETE SET NULL
 );
 
+-- Create the Sales table
 CREATE TABLE Sales (
     id SERIAL PRIMARY KEY,
     drugCode VARCHAR(50),
