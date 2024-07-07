9fb0db22b4fbd9cc365d74a2e05dfe99982efaf8
Jtandoh01 <tandoh24700@gmail.com>
2024-07-07 19:22:35 +0000

    documenting suppliers and supplier services

--- app/src/main/java/com/pharmacy/services/SupplierService.java
+++ app/src/main/java/com/pharmacy/services/SupplierService.java
@@ -1,3 +1,4 @@
+
 package com.pharmacy.services;
 
 import com.pharmacy.models.Supplier;
@@ -8,6 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierService {
+
+    /**
+     * Adds a new supplier to the database.
+     *
+     * @param supplier The Supplier object to be added.
+     */
     public void addSupplier(Supplier supplier) {
         String query = "INSERT INTO Suppliers (name, location) VALUES (?, ?)";
         try (Connection connection = Database.getDataSource().getConnection()) {
@@ -21,6 +28,12 @@ public class SupplierService {
         }
     }
 
+    /**
+     * Searches for suppliers by their location.
+     *
+     * @param location The location to search for suppliers.
+     * @return A list of Supplier objects matching the location.
+     */
     public List<Supplier> searchSuppliersByLocation(String location) {
         List<Supplier> suppliers = new ArrayList<>();
         String query = "SELECT * FROM Suppliers WHERE location = ?";
@@ -37,6 +50,11 @@ public class SupplierService {
         return suppliers;
     }
 
+    /**
+     * Retrieves all suppliers from the database.
+     *
+     * @return A list of Supplier objects.
+     */
     public List<Supplier> getAllSuppliers() {
         List<Supplier> suppliers = new ArrayList<>();
         String query = "SELECT * FROM Suppliers";
@@ -46,9 +64,7 @@ public class SupplierService {
                      ResultSet rs = stmt.executeQuery(query)) {
                     while (rs.next()) {
                         suppliers.add(new Supplier(rs.getInt("id"), rs.getString("name"), rs.getString("location")));
-                    
                     }
-
                 }
             } else {
                 System.err.println("Failed to make connection to the database.");
@@ -58,13 +74,16 @@ public class SupplierService {
         }
         return suppliers;
     }
-    
 
-
+    /**
+     * Updates an existing supplier in the database.
+     *
+     * @param supplier The Supplier object containing updated information.
+     */
     public void updateSupplier(Supplier supplier) {
         String query = "UPDATE Suppliers SET name = ?, location = ? WHERE id = ?";
         try (Connection conn = Database.getDataSource().getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(query)) {
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, supplier.getName());
             pstmt.setString(2, supplier.getLocation());
             pstmt.setInt(3, supplier.getId());
@@ -74,6 +93,11 @@ public class SupplierService {
         }
     }
 
+    /**
+     * Deletes a supplier from the database based on their ID.
+     *
+     * @param id The ID of the supplier to be deleted.
+     */
     public void deleteSupplier(int id) {
         String query = "DELETE FROM Suppliers WHERE id = ?";
         try (Connection conn = Database.getDataSource().getConnection();
